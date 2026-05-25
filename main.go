5e31f9fe19036e305b253fc19a07d879968b85a0
kchakka-pixel <kchakka@gitam.in>
2026-05-26 00:08:03 +0530

    Enabled CORS

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/cors"
@@ -14,46 +15,56 @@ func main() {
 
 	initTables()
 
-	// AUTH
 	http.HandleFunc("/signup", LoggingMiddleware(Signup))
 	http.HandleFunc("/login", LoggingMiddleware(Login))
 	http.HandleFunc("/logout", LoggingMiddleware(AuthMiddleware(Logout)))
 
-	// ADMIN
 	http.HandleFunc("/admin/waiters", LoggingMiddleware(AuthMiddleware(GetWaiters)))
 	http.HandleFunc("/admin/stats", LoggingMiddleware(AuthMiddleware(GetWaiterStats)))
 	http.HandleFunc("/admin/assign-table", LoggingMiddleware(AuthMiddleware(AssignTable)))
 	http.HandleFunc("/admin/delete-waiter", LoggingMiddleware(AuthMiddleware(DeleteWaiter)))
 
-	// WAITER
 	http.HandleFunc("/waiter/status", LoggingMiddleware(AuthMiddleware(UpdateAvailability)))
 
-	// TABLE
 	http.HandleFunc("/table/status", LoggingMiddleware(AuthMiddleware(UpdateTableStatus)))
 	http.HandleFunc("/tables", LoggingMiddleware(GetTables))
 	http.HandleFunc("/queue", LoggingMiddleware(GetQueue))
 
-	// WEBSOCKET
 	http.HandleFunc("/ws", HandleWebSocket)
 
-	// METRICS
 	http.Handle("/metrics", promhttp.Handler())
-	// HEALTH CHECK
+
 	http.HandleFunc("/health", HealthCheck)
 
-	//  SERVE FRONTEND
 	fs := http.FileServer(http.Dir("./frontend"))
 	http.Handle("/", fs)
 
-	// Setup CORS
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"}, // allow all origins
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"*"},
+		AllowedOrigins: []string{
+			"*",
+		},
+		AllowedMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+		AllowedHeaders: []string{
+			"*",
+		},
 		AllowCredentials: true,
 	})
+
 	handler := c.Handler(http.DefaultServeMux)
 
-	log.Println("Server running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Server running on port :", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
